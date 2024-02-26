778529e6d12cc28f182e85412c3abdd70623ad67
David Arias <davidariascalderon04@gmail.com>
2024-02-26 15:53:21 -0500

    sql para docker

--- api/database/up.sql
+++ api/database/up.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS cliente;
+--Crear la base de datos
+CREATE DATABASE factura;
 
+-- Crear tabla CLIENTE
 CREATE TABLE CLIENTE (
     id_cliente SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
@@ -7,3 +9,29 @@ CREATE TABLE CLIENTE (
     identificacion VARCHAR(20) NOT NULL,
     correo VARCHAR(100) NOT NULL
 );
+
+-- Crear tabla FACTURA
+CREATE TABLE FACTURA (
+    id_factura SERIAL PRIMARY KEY,
+    fecha DATE NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    id_cliente INTEGER REFERENCES CLIENTE(id_cliente) ON DELETE CASCADE
+);
+
+-- Crear tabla ITEM
+CREATE TABLE ITEM (
+    id_item SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    valor NUMERIC(10, 2) NOT NULL
+);
+
+--Crear tabla de rompimiento
+CREATE TABLE FACTURA_ITEM (
+    id_factura_item SERIAL PRIMARY KEY,
+    id_factura INTEGER REFERENCES FACTURA(id_factura) ON DELETE CASCADE,
+    id_item INTEGER REFERENCES ITEM(id_item) ON DELETE CASCADE
+);
+
+-- Crear índices únicos para evitar duplicados en la relación muchos a muchos
+CREATE UNIQUE INDEX idx_factura_item ON FACTURA_ITEM (id_factura, id_item);
