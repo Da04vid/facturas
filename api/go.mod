b1f700f438c38a740c8157f092e634df27ec0687
DAVID PORTATIL <Admin@DESKTOP3QH25C.a>
2024-02-26 08:43:12 -0800

    puesta en marcha de servidor

--- api/go.mod
+++ api/go.mod
@@ -3,7 +3,7 @@ module facturas
 go 1.18
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
---
+++ api/server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config{
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error){
+	if config.Port ==  ""{
+		return nil, errors.New("Puerto requerido")
+	}
+	if config.JWTSecret ==  ""{
+		return nil, errors.New("Clave secreta requerida")
+	}
+	if config.DatabaseUrl ==  ""{
+		return nil, errors.New("Base de datos requerida")
+	}
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server,r *mux.Router)){
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Print("Inicializando servidor", b.Config().Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
